cc9683a8bd202be4755acfaed9e750dcdc7d5db5
Ayako Woollan <ayako.woollan@gmail.com>
2022-05-03 19:50:56 +1000

    updating user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,27 +1,52 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
+const recipeSchema = require('./Recipe');
 
 const userSchema = new Schema(
     {
-        firstName: {
+        username: {
             type: String,
             required: true,
-            max_length: 50,
+            unique: true,
         },
-        lastName: {
+        email: {
             type: String,
             required: true,
-            max_length: 50,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
         },
-
-        recipes: {
-            type: Schema.Types.ObjectId,
-            ref: 'Recipe',
-        }
+        password: {
+            type: String,
+            required: true,
+        },
+        savedRecipe: [recipeSchema],
     },
     {
         toJSON: {
-            getters: true,
+            virtuals: true,
         },
     }
 );
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+userSchema.virtual('recipeCount').get(function () {
+    return this.savedRecipes.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
