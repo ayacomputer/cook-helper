4a67d399e72b58661576a0319fc9e1735c62dea9
Ayako Woollan <ayako.woollan@gmail.com>
2022-05-05 00:13:24 +1000

    updating resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,63 @@
-const { Cooking, Recipe } = require('../models');
+const { createSourceEventStream } = require('graphql');
+const { Cooking, Recipe, User } = require('../models');
 
 const resolvers = {
   Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    getRecipes: async (parent, args) => {
+      return await Recipe.find({});
+    },
+    getOneRecipe: {
+      return await Recipe.findOne({});
 
-  };
+    }
+  },
+
+  mutation: {
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    createUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveRecipe: async (parent, { input }, context) => {
+
+      if (context.user) {
+        return await User.findOneAndUpdate({ _id: context.user._id },
+          { $addToSet: { savedRecipes: input } },
+          { new: true });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeRecipe: async (parent, { recipeId }, context) => {
+
+      if (context.user) {
+        return await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedRecipes: { recipeId } } },
+          { new: true });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+
+  }
 
   module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -62,9 +62,6 @@ const typeDefs = gql`
  
 }
 
-
-
-
 `;
 
 module.exports = typeDefs;
