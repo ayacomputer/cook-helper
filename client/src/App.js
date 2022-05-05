dd64216a98f4212ab95d3758b4056f29c176ef03
Ayako Woollan <ayako.woollan@gmail.com>
2022-05-05 17:04:57 +1000

    uploading app.js and basic pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
 
+import { setContext } from '@apollo/client/link/context';
+
+import Dashboard from './pages/dashboard';
+import Login from './pages/login';
+import CreateRecipe from './pages/createRecipe'
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -15,9 +40,17 @@ function App() {
         <div className="flex-column justify-center align-center min-100-vh bg-primary">
           <Routes>
             <Route
-              path="*"
-              element={< />}
-                />
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/"
+              element={<Dashboard />}
+            />
+            <Route
+              path="/createRecipe"
+              element={<CreateRecipe />}
+            />
           </Routes>
         </div>
       </Router>
