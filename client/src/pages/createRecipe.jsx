8d7dd60c6b4b88f9743004f6081d21a4fa36a7c5
Ayako Woollan <ayako.woollan@gmail.com>
2022-05-11 14:07:01 +1000

    updating createrecipe page(still editing)

--- client/src/pages/createRecipe.jsx
+++ client/src/pages/createRecipe.jsx
@@ -1,12 +1,22 @@
+import { Container, Typography } from '@mui/material';
 import React from 'react';
 import NavBar from '../layouts/NavBar';
+import { CREATE_RECIPE } from '../utils/mutations';
 
 export default function CreateRecipe() {
+    const [userFormData, setUserFormData] = useState({ image: '', name: '', ingredients: '', steps: '', totalTime: '', serves: '' });
+    const [createRecipe] = useMutation(CREATE_RECIPE);
+
 
 
     return (
         <>
             <NavBar />
+            <Container>
+                <Typography>Create Recipe</Typography>
+                <Form>
+                </Form>
+            </Container>
 
         </>
 
