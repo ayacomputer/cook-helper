0443db4eb4d2029820c2af45d02c2fd948dbb2c6
Ayako Woollan <ayako.woollan@gmail.com>
2022-08-09 17:44:44 +1000

    removing mongoose options no longer supported

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/cook-helper', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+}, err => {
+  if (err) throw err;
+  console.log('Connected to MongoDB!!!')
+}
+);
 
 module.exports = mongoose.connection;
